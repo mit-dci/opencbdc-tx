404e1b211899b38259d8720d2d433ebf2145f66e
Sam Stuewe <stuewe@mit.edu>
2022-05-06 15:52:12 -0500

    Add helpers for creating Pedersen Commitments
    Includes creating commitments directly as well as a facility for
    automatically creating an “xonly” Pedersen Commitment.
    
    xonly Pedersen Commitments are a work-around to avoid a larger code-
    delta for this solution. In particular, it will allow us to avoid
    changing the size of a UHS ID.
    
    Signed-off-by: Sam Stuewe <stuewe@mit.edu>

---
+++ src/util/common/commitment.cpp
@@ -0,0 +1,77 @@
+// Copyright (c) 2021 MIT Digital Currency Initiative,
+//                    Federal Reserve Bank of Boston
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "keys.hpp"
+#include "commitment.hpp"
+
+#include "crypto/sha256.h"
+
+#include <cstring>
+#include <vector>
+#include <array>
+#include <sstream>
+
+#include <secp256k1_generator.h>
+
+namespace cbdc {
+    auto commit(const secp256k1_context* ctx, uint64_t value, const hash_t blind) -> std::optional<secp256k1_pedersen_commitment> {
+        secp256k1_pedersen_commitment commit{};
+        auto res = secp256k1_pedersen_commit(ctx, &commit, blind.data(), value, secp256k1_generator_h);
+        if (res != 1) {
+            return std::nullopt;
+        }
+
+        return commit;
+    }
+
+    auto serialize_commitment(const secp256k1_context* ctx, secp256k1_pedersen_commitment comm) -> commitment_t {
+        commitment_t c{};
+        secp256k1_pedersen_commitment_serialize(ctx, c.data(), &comm);
+        return c;
+    }
+
+    auto make_commitment(const secp256k1_context* ctx, uint64_t value, const hash_t blind) -> std::optional<commitment_t> {
+        auto comm = commit(ctx, value, blind);
+        if (!comm.has_value()) {
+            return std::nullopt;
+        }
+
+        return serialize_commitment(ctx, comm.value());
+    }
+
+    auto make_xonly_commitment(const secp256k1_context* ctx, uint64_t value, const hash_t blind) -> std::optional<hash_t> {
+        auto lemma = make_commitment(ctx, value, blind);
+        if (!lemma.has_value() || lemma.value().front() != 0x08) {
+            return std::nullopt;
+        }
+
+        hash_t commitment{};
+        memcpy(commitment.data(), lemma.value().data() + 1, hash_size);
+
+        return commitment;
+    }
+
+    auto deserialize_commitment(const secp256k1_context* ctx, commitment_t comm) -> std::optional<secp256k1_pedersen_commitment> {
+        secp256k1_pedersen_commitment commitment{};
+        if (secp256k1_pedersen_commitment_parse(ctx, &commitment, comm.data()) != 1) {
+            return std::nullopt;
+        }
+
+        return commitment;
+    }
+
+    auto expand_xonly_commitment(const secp256k1_context* ctx, hash_t comm) -> std::optional<secp256k1_pedersen_commitment> {
+        commitment_t commit{};
+        commit[0] = 0x08;
+        std::memcpy(commit.data() + 1, comm.data(), hash_size);
+
+        secp256k1_pedersen_commitment commitment{};
+        if (secp256k1_pedersen_commitment_parse(ctx, &commitment, commit.data()) != 1) {
+            return std::nullopt;
+        }
+
+        return commitment;
+    }
+}
---
+++ src/util/common/commitment.hpp
@@ -0,0 +1,83 @@
+// Copyright (c) 2021 MIT Digital Currency Initiative,
+//                    Federal Reserve Bank of Boston
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef OPENCBDC_TX_SRC_COMMON_COMMITMENT_H_
+#define OPENCBDC_TX_SRC_COMMON_COMMITMENT_H_
+
+#include "hash.hpp"
+#include "keys.hpp"
+
+#include <optional>
+
+#include <secp256k1_generator.h>
+
+namespace cbdc {
+    /// Creates a Pedersen commitment
+    ///
+    /// \param ctx secp256k1 context initialized for signing and commitment
+    /// \param value the value to commit to
+    /// \param blinder a 32-byte blinding factor
+    /// \return a pedersen commitment, or nullopt if making the commitment
+    ///         failed
+    auto commit(const secp256k1_context* ctx, uint64_t value, const hash_t blind) -> std::optional<secp256k1_pedersen_commitment>;
+
+    /// Serializes a Pedersen commitment
+    ///
+    /// \param ctx secp256k1 context initialized for signing and commitment
+    /// \param comm the commitment to serialize
+    /// \return a serialized pedersen commitment
+    auto serialize_commitment(const secp256k1_context* ctx, secp256k1_pedersen_commitment comm) -> commitment_t;
+
+    /// Creates and serializes a Pedersen commitment
+    ///
+    /// A shortcut to to \ref commit and \ref serialize_commit
+    ///
+    /// \param ctx secp256k1 context initialized for signing and commitment
+    /// \param value the value to commit to
+    /// \param blinder a 32-byte blinding factor
+    /// \return a serialized pedersen commitment, or nullopt if making the
+    ///         commitment failed
+    auto make_commitment(const secp256k1_context* ctx, uint64_t value, const hash_t blinder) -> std::optional<commitment_t>;
+
+    /// Attempts to create a Pedersen commitment and serialize it into 32-bytes
+    ///
+    /// We assume the y-coordinate of a 32-byte Pedersen commitment is positve.
+    /// If the blinder provided results in a Pedersen commitment with a
+    /// postive y-coordinate, then this method will discard the leading byte
+    /// (which normally disambiguates the y-coordinate) and return the
+    /// remaining 32 bytes.
+    ///
+    /// If the resulting Pedersen commitment has a negative y-coordinate, then
+    /// this method returns std::nullopt.
+    ///
+    /// Practically, you would expect to need to call this method multiple
+    /// times (on average, twice) with different, random blinding factors to
+    /// succeed in generating a 32-byte-representable Pedersen commitment.
+    ///
+    /// \param ctx secp256k1 context initialized for signing and commitment
+    /// \param value the value to commit to
+    /// \param blinder a 32-byte blinding factor
+    /// \return a serialized pedersen commitment, or nullopt if the commitment
+    ///         could not be represented in 32 bytes
+    auto make_xonly_commitment(const secp256k1_context* ctx, uint64_t value, const hash_t blinder) -> std::optional<hash_t>;
+
+    /// Attempts to deserialize a Pedersen commitment
+    ///
+    /// \param ctx secp256k1 context initialized for signing and commitment
+    /// \param comm a 33-byte serialized Pedersen commitment
+    /// \return the deserialized commitment, or std::nullopt if the input
+    ///         was invalid
+    auto deserialize_commitment(const secp256k1_context* ctx, commitment_t comm) -> std::optional<secp256k1_pedersen_commitment>;
+
+    /// Attempts to deserialize an xonly Pedersen commitment
+    ///
+    /// \param ctx secp256k1 context initialized for signing and commitment
+    /// \param comm a 32-byte serialized x-only Pedersen commitment
+    /// \return the deserialized commitment, or std::nullopt if the input
+    ///         was invalid
+    auto expand_xonly_commitment(const secp256k1_context* ctx, hash_t comm) -> std::optional<secp256k1_pedersen_commitment>;
+}
+
+#endif // OPENCBDC_TX_SRC_COMMON_COMMITMENT_H_
