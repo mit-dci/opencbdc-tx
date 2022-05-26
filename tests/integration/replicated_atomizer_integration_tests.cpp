283ee74c8d431b31abf9fbab4dcc1d9cb18b85a7
Alexander Jung <104335080+AlexRamRam@users.noreply.github.com>
2022-05-25 21:19:45 -0400

    Test atomizer raft node recovery using snapshot
    - Creation of a new test item
    `replicated_atomizer_integration_tests.raftnode_crash_recover` which
    exercises the snapshot feature and partially validates its workings by
    forcing a crash and recovery. Note that the test itself does not
    explicitly validate the state of the atomizer node after recovery.
    - replicated_atomizer.cfg: add `snapshot_distance=2` to activate NuRaft snapshot feature for testing
    
    Fix #108
    Signed-off-by: Alexander Jung <104335080+AlexRamRam@users.noreply.github.com>

--- tests/integration/replicated_atomizer_integration_tests.cpp
+++ tests/integration/replicated_atomizer_integration_tests.cpp
@@ -79,6 +79,12 @@ class replicated_atomizer_integration_tests : public ::testing::Test {
             m_block_client_thread.join();
         }
 
+        // Before cleaning up atomizer-managed files, must destroy the atomizer
+        // controllers (i.e. wait for their completion):
+        for(auto& contptr : m_ctls) {
+            contptr.reset(nullptr);
+        }
+
         std::filesystem::remove_all("archiver0_db");
         for(auto i = 0ULL; i < m_atomizer_count; ++i) {
             std::filesystem::remove_all("atomizer_raft_log_"
@@ -136,4 +142,57 @@ TEST_F(replicated_atomizer_integration_tests,
         cbdc::atomizer::tx_notify_request{tx, {}, 0}}));
 
     expect_tx(tx, std::chrono::seconds(5));
+}
+
+TEST_F(replicated_atomizer_integration_tests, raftnode_crash_recover) {
+    // Kill the Leader atomizer node:
+    const auto killidx = 0;
+    m_logger->info("Killing 0th (and leader) atomizer raft node - to be "
+                   "re-installed later");
+    m_ctls[killidx].reset(nullptr);
+
+    // This test assumes that snapshot_distance value has been set thusly:
+    ASSERT_EQ(m_opts.m_snapshot_distance, 2);
+
+    // Wait until the raft-cluster has re-configured leadership:
+    m_logger->info("Waiting for atomizer raft cluster to reconnect with new "
+                   "leader ");
+    int intv = 0;
+    for(; !m_cluster.connected_to_one(); ++intv) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    std::cout << std::endl;
+    m_logger->info("DONE: atomizer raft cluster has reconnected with new"
+                   " leader (after ~",
+                   100 * intv,
+                   " milliseconds).");
+
+    // Sending a number of transactions (>=snapshot_distance) to ensure
+    // snapshot is being taken:
+    const auto tx = cbdc::test::simple_tx({'a'}, {}, {{{'c'}}});
+    ASSERT_TRUE(m_cluster.send_to_one(cbdc::atomizer::request{
+        cbdc::atomizer::tx_notify_request{tx, {}, 0}}));
+    expect_tx(tx, std::chrono::seconds(5));
+
+    const auto tx2 = cbdc::test::simple_tx({'b'}, {}, {{{'d'}}});
+    ASSERT_TRUE(m_cluster.send_to_one(cbdc::atomizer::request{
+        cbdc::atomizer::tx_notify_request{tx2, {}, 0}}));
+    expect_tx(tx2, std::chrono::seconds(5));
+
+    const auto tx3 = cbdc::test::simple_tx({'c'}, {}, {{{'e'}}});
+    ASSERT_TRUE(m_cluster.send_to_one(cbdc::atomizer::request{
+        cbdc::atomizer::tx_notify_request{tx3, {}, 0}}));
+    expect_tx(tx3, std::chrono::seconds(5));
+
+    // Add node back:
+    m_logger->info("Reinstall the 0th atomizer raft node");
+    m_ctls[killidx]
+        = (std::make_unique<cbdc::atomizer::controller>(0, m_opts, m_logger));
+    ASSERT_TRUE(m_ctls[killidx]->init());
+
+    // Send/confirm another transaction
+    const auto tx4 = cbdc::test::simple_tx({'d'}, {}, {{{'f'}}});
+    ASSERT_TRUE(m_cluster.send_to_one(cbdc::atomizer::request{
+        cbdc::atomizer::tx_notify_request{tx4, {}, 0}}));
+    expect_tx(tx4, std::chrono::seconds(5));
 }
