b0a5400ca5e581857b410cc213338baeaddc5ed3
Michael L. Szulczewski <mszulczewski@mitre.org>
2022-06-06 20:56:06 -0400

    Add flag to skip coverage measurement
    With its previous implementation, test.sh would always run
    tests and measure coverage.  This commit adds a command-line flag that allows
    the user to only run the tests.
    
    Signed-off-by: Michael L. Szulczewski <mszulczewski@mitre.org>

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,45 @@
 # Exit script on failure.
 set -e
 
+function usage() { echo "
+USAGE:
+    ./test.sh
+
+DESCRIPTION:
+    This script runs unit and integration tests and measures test coverage. 
+
+FLAGS: 
+    -nc, --no-coverage              Do not measure test coverage.
+                                    Default:  false
+    -h, --help                      Show usage.
+	"
+}
+
+MEASURE_COVERAGE="true"
+while [[ $# -gt 0 ]]
+do
+    case $1 in
+        -h|--help)
+            usage
+            exit 0
+            ;; 
+        -nc|--no-coverage)
+            MEASURE_COVERAGE="false"
+            shift
+            ;; 
+        -*)
+            echo "ERROR:  unknown command-line option '${1}'."
+            usage
+            exit 1
+            ;;
+        *)
+            echo "ERROR:  unknown command-line option '${1}'."
+            usage
+            exit 1
+            ;;
+    esac
+done
+
 if [ -z ${BUILD_DIR+x} ]; then
     export BUILD_DIR=build
 fi
@@ -11,22 +50,31 @@ run_test_suite () {
     find . -name '*.gcda' -exec rm {} \;
     "$PWD"/"$1"
 
-    LOCATION=$2
-    rm -rf $LOCATION
-    mkdir -p $LOCATION
-    find . \( -name '*.gcno' -or -name '*.gcda' \) -and -not -path '*coverage*' -exec rsync -R \{\} "$LOCATION" \;
-    cd $LOCATION
+    if [[ "$MEASURE_COVERAGE" == "true" ]]
+    then
+        echo "Checking test coverage."
+        LOCATION=$2
+        rm -rf "$LOCATION"
+        mkdir -p "$LOCATION"
+        find . \( -name '*.gcno' -or -name '*.gcda' \) \
+            -and -not -path '*coverage*' -exec rsync -R \{\} "$LOCATION" \;
+        cd "$LOCATION"
 
-    lcov -c -i -d . -o base.info --rc lcov_branch_coverage=1
-    lcov -c -d . -o test.info --rc lcov_branch_coverage=1
-    lcov -a base.info -a test.info -o total.info --rc lcov_branch_coverage=1
-    lcov --remove total.info '*/3rdparty/*' '*/tests/*' '*/tools/*' \
-        -o trimmed.info --rc lcov_branch_coverage=1
-    lcov --extract trimmed.info '*/src/*' -o cov.info \
-        --rc lcov_branch_coverage=1
-    genhtml cov.info -o output --rc genhtml_branch_coverage=1
+        lcov -c -i -d . -o base.info --rc lcov_branch_coverage=1
+        lcov -c -d . -o test.info --rc lcov_branch_coverage=1
+        lcov -a base.info -a test.info -o total.info --rc lcov_branch_coverage=1
+        lcov --remove total.info '*/3rdparty/*' '*/tests/*' '*/tools/*' \
+            -o trimmed.info --rc lcov_branch_coverage=1
+        lcov --extract trimmed.info '*/src/*' -o cov.info \
+            --rc lcov_branch_coverage=1
+        genhtml cov.info -o output --rc genhtml_branch_coverage=1
 
-    cd ../..
+        cd ..
+    else
+        echo "Skipping coverage measurement."
+    fi
+
+    cd ..
 }
 
 echo "Running unit tests..."
