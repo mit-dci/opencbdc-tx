c12f6cd8482d07d182f85b3fea535b65f603a65c
Michael L. Szulczewski <mszulczewski@mitre.org>
2022-06-06 20:44:52 -0400

    Enclose variable references in double quotes
    The Advanced Bash-Scripting Guide (Ch.5) recommends enclosing variables
    in double quotes to prevent word splitting and globbing.
    
    Signed-off-by: Michael L. Szulczewski <mszulczewski@mitre.org>

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# exit script on failure
+# Exit script on failure.
 set -e
 
 if [ -z ${BUILD_DIR+x} ]; then
@@ -7,15 +7,15 @@ if [ -z ${BUILD_DIR+x} ]; then
 fi
 
 run_test_suite () {
-    cd $BUILD_DIR
+    cd "$BUILD_DIR"
     find . -name '*.gcda' -exec rm {} \;
-    $PWD/$1
+    "$PWD"/"$1"
 
     LOCATION=$2
-    rm -rf $LOCATION
-    mkdir -p $LOCATION
+    rm -rf "$LOCATION"
+    mkdir -p "$LOCATION"
     find . \( -name '*.gcno' -or -name '*.gcda' \) -and -not -path '*coverage*' -exec cp --parents \{\} "$LOCATION" \;
-    cd $LOCATION
+    cd "$LOCATION"
 
     lcov -c -i -d . -o base.info --rc lcov_branch_coverage=1
     lcov -c -d . -o test.info --rc lcov_branch_coverage=1
@@ -31,5 +31,5 @@ echo "Running unit tests..."
 run_test_suite "tests/unit/run_unit_tests" "unit_tests_coverage"
 
 echo "Running integration tests..."
-cp tests/integration/*.cfg $BUILD_DIR
+cp tests/integration/*.cfg "$BUILD_DIR"
 run_test_suite "tests/integration/run_integration_tests" "integration_tests_coverage"
