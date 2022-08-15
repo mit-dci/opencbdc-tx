20d7ace85af4c53db9f831eafec4ed6487c08111
Michael L. Szulczewski <mszulczewski@mitre.org>
2022-08-15 01:16:39 -0400

    Enable atomizer to wait until synched with watchtowers
    Previously, the atomizer controller would make one attempt to connect to
    watchtowers and would return false if the attempt failed.  This required
    watchtowers to be initialized before atomizers, which required adding a
    sleep to the atomizer end-to-end integration test so the watchtowers had time
    to initialize.  This change enables the atomizer to make multiple attempts to
    connect to watchtowers and precludes the need for the sleep in the
    integration test.
    
    Signed-off-by: Michael L. Szulczewski <mszulczewski@mitre.org>

--- tests/integration/atomizer_end_to_end_test.cpp
+++ tests/integration/atomizer_end_to_end_test.cpp
@@ -47,7 +47,7 @@ class atomizer_end_to_end_test : public ::testing::Test {
             ASSERT_TRUE(m_ctl_watchtower->init());
         });
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        // std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
         ASSERT_TRUE(m_ctl_atomizer->init());
         ASSERT_TRUE(m_ctl_archiver->init());
