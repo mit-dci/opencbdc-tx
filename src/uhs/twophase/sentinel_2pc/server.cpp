6183b2a557a737a5f1e081e2e44e862f036bec89
Gert-Jaap Glasbergen <gertjaap@wadagso.com>
2022-09-07 08:17:14 +0200

    Prioritize cross-sentinel attestation requests
    Signed-off-by: Gert-Jaap Glasbergen <gertjaap@wadagso.com>

--- src/uhs/twophase/sentinel_2pc/server.cpp
+++ src/uhs/twophase/sentinel_2pc/server.cpp
@@ -11,22 +11,44 @@ namespace cbdc::sentinel::rpc {
         std::unique_ptr<cbdc::rpc::async_server<request, response>> srv)
         : m_impl(impl),
           m_srv(std::move(srv)) {
+        m_processing_thread = std::thread([this]() {
+            process();
+        });
         m_srv->register_handler_callback(
             [&](const request& req,
                 async_interface::result_callback_type callback) {
-                auto res = std::visit(
-                    overloaded{[&](execute_request e_req) {
-                                   return m_impl->execute_transaction(
-                                       std::move(e_req),
-                                       callback);
-                               },
-                               [&](validate_request v_req) {
-                                   return m_impl->validate_transaction(
-                                       std::move(v_req),
-                                       callback);
-                               }},
-                    req);
-                return res;
+                auto req_item = request_queue_t{req, callback};
+                m_request_queue.push(req_item);
+                return true;
             });
+    }
+    bool operator<(const request_queue_t& a, const request_queue_t& b) {
+        // Prioritize validate requests over execute requests
+        return (std::holds_alternative<validate_request>(a.m_req)
+                && std::holds_alternative<execute_request>(b.m_req));
+    }
+    async_server::~async_server() {
+        m_running = false;
+        if(m_processing_thread.joinable()) {
+            m_processing_thread.join();
+        }
+    }
+    void async_server::process() {
+        auto q = request_queue_t();
+        while(m_running) {
+            if(m_request_queue.pop(q)) {
+                std::visit(overloaded{[&](execute_request e_req) {
+                                          return m_impl->execute_transaction(
+                                              std::move(e_req),
+                                              q.m_cb);
+                                      },
+                                      [&](validate_request v_req) {
+                                          return m_impl->validate_transaction(
+                                              std::move(v_req),
+                                              q.m_cb);
+                                      }},
+                           q.m_req);
+            }
+        }
     }
 }
--- src/uhs/twophase/sentinel_2pc/server.hpp
+++ src/uhs/twophase/sentinel_2pc/server.hpp
@@ -8,10 +8,17 @@
 
 #include "uhs/sentinel/async_interface.hpp"
 #include "uhs/transaction/messages.hpp"
+#include "util/common/blocking_queue.hpp"
 #include "util/rpc/async_server.hpp"
 #include "util/rpc/format.hpp"
 
 namespace cbdc::sentinel::rpc {
+    struct request_queue_t {
+        request m_req;
+        async_interface::result_callback_type m_cb;
+    };
+
+    bool operator<(const request_queue_t& a, const request_queue_t& b);
     /// Asynchronous RPC server for a sentinel.
     class async_server {
       public:
@@ -25,9 +32,18 @@ namespace cbdc::sentinel::rpc {
                                    //      implementation
             std::unique_ptr<cbdc::rpc::async_server<request, response>> srv);
 
+        ~async_server();
+
       private:
+        void process();
+
         async_interface* m_impl;
         std::unique_ptr<cbdc::rpc::async_server<request, response>> m_srv;
+
+        blocking_priority_queue<request_queue_t, std::less<request_queue_t>>
+            m_request_queue{};
+        std::thread m_processing_thread;
+        std::atomic<bool> m_running = true;
     };
 }
 
