3a33f7e0edf1b1896592f1f4cac688c595df7f7a
paparuch <paparuch@0day.im>
2022-09-10 01:48:20 +0000

    Moved decode_address from client-cli to client
    Signed-off-by: paparuch <paparuch@0day.im>
    Co-authored-by: Michael Maurer <maurer.mi@northeastern.edu>

--- src/uhs/client/client.cpp
+++ src/uhs/client/client.cpp
@@ -5,6 +5,8 @@
 
 #include "client.hpp"
 
+#include "bech32/bech32.h"
+#include "bech32/util/strencodings.h"
 #include "crypto/sha256.h"
 #include "uhs/sentinel/format.hpp"
 #include "uhs/transaction/messages.hpp"
@@ -20,6 +22,42 @@
 #include <utility>
 
 namespace cbdc {
+
+    namespace address {
+        auto decode(const std::string& addr_str)
+            -> std::optional<cbdc::hash_t> {
+            // TODO: if/when bech32m is merged into Bitcoin Core, switch to
+            // that.
+            //       see: https://github.com/bitcoin/bitcoin/pull/20861
+            const auto [hrp, enc_data] = bech32::Decode(addr_str);
+            if(hrp != cbdc::config::bech32_hrp) {
+                std::cout << "Invalid address encoding" << std::endl;
+                return std::nullopt;
+            }
+            auto data = std::vector<uint8_t>();
+            ConvertBits<bech32_bits_per_symbol, bits_per_byte, false>(
+                [&](uint8_t c) {
+                    data.push_back(c);
+                },
+                enc_data.begin(),
+                enc_data.end());
+
+            auto pubkey = cbdc::hash_t();
+            if(data[0]
+                   != static_cast<uint8_t>(
+                       cbdc::client::address_type::public_key)
+               || data.size() != pubkey.size() + 1) {
+                std::cout << "Address is not a supported type" << std::endl;
+                return std::nullopt;
+            }
+
+            data.erase(data.begin());
+            std::copy_n(data.begin(), pubkey.size(), pubkey.begin());
+
+            return pubkey;
+        }
+    }
+
     client::client(cbdc::config::options opts,
                    std::shared_ptr<logging::log> logger,
                    std::string wallet_file,
