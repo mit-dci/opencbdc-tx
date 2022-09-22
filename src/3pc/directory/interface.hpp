40bf5b85c781df81a048f22fba7ac5e86773b141
James Lovejoy <jameslovejoy1@gmail.com>
2022-09-22 14:45:36 -0400

    Implement directory library
    Signed-off-by: James Lovejoy <jameslovejoy1@gmail.com>

---
+++ src/3pc/directory/interface.hpp
@@ -0,0 +1,40 @@
+// Copyright (c) 2021 MIT Digital Currency Initiative,
+//                    Federal Reserve Bank of Boston
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef CBDC_UNIVERSE0_SRC_3PC_DIRECTORY_INTERFACE_H_
+#define CBDC_UNIVERSE0_SRC_3PC_DIRECTORY_INTERFACE_H_
+
+#include "3pc/runtime_locking_shard/interface.hpp"
+
+namespace cbdc::threepc::directory {
+    /// Interface for a directory. Maps keys to shard IDs.
+    class interface {
+      public:
+        virtual ~interface() = default;
+
+        interface() = default;
+        interface(const interface&) = delete;
+        auto operator=(const interface&) -> interface& = delete;
+        interface(interface&&) = delete;
+        auto operator=(interface&&) -> interface& = delete;
+
+        /// Key location return type. Shard ID where key is located.
+        using key_location_return_type = uint64_t;
+        /// Callback function type for key location result.
+        using key_location_callback_type
+            = std::function<void(key_location_return_type)>;
+
+        /// Returns the shard ID responsible for the given key.
+        /// \param key key to locate.
+        /// \param result_callback function to call with key location.
+        /// \return true if the operation was initiated successfully.
+        virtual auto key_location(runtime_locking_shard::key_type key,
+                                  key_location_callback_type result_callback)
+            -> bool
+            = 0;
+    };
+}
+
+#endif
