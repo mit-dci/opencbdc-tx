eb9dd861b475cc0eff419ff34ba6fe52b7e5498f
Sam Stuewe <stuewe@mit.edu>
2023-01-13 11:05:47 -0600

    fix: ensure all docker images get built
    16bd61aba5edabf089fe7894dec7c81956e41eaf errantly stopped building
    the `opencbdc-tx-builder` image. This change ensures that the builder
    image, as well as `-atomizer` are also built by
    `./scripts/build-docker.sh`.
    
    Signed-off-by: Sam Stuewe <stuewe@mit.edu>

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -3,12 +3,16 @@
 set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-DOCKER_IMAGE_TAG_TWOPHASE=${DOCKER_IMAGE_TAG:-opencbdc-tx-twophase}
 DOCKER_IMAGE_TAG_BASE=${DOCKER_IMAGE_TAG:-opencbdc-tx-base}
+DOCKER_IMAGE_TAG_BUILDER=${DOCKER_IMAGE_TAG:-opencbdc-tx-builder}
+DOCKER_IMAGE_TAG_ATOMIZER=${DOCKER_IMAGE_TAG:-opencbdc-tx-atomizer}
+DOCKER_IMAGE_TAG_TWOPHASE=${DOCKER_IMAGE_TAG:-opencbdc-tx-twophase}
 
 # Update submodules before building
 git submodule init && git submodule update
 
 # Build docker image
 docker build --target base -t $DOCKER_IMAGE_TAG_BASE -f $SCRIPT_DIR/../Dockerfile $SCRIPT_DIR/..
+docker build --target builder --build-arg BASE_IMAGE=base -t $DOCKER_IMAGE_TAG_BUILDER -f $SCRIPT_DIR/../Dockerfile $SCRIPT_DIR/..
 docker build --target twophase --build-arg BASE_IMAGE=base -t $DOCKER_IMAGE_TAG_TWOPHASE -f $SCRIPT_DIR/../Dockerfile $SCRIPT_DIR/..
+docker build --target atomizer --build-arg BASE_IMAGE=base -t $DOCKER_IMAGE_TAG_ATOMIZER -f $SCRIPT_DIR/../Dockerfile $SCRIPT_DIR/..
