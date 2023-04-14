5bc9c0184996694f7eb04c3ad8fed67b5ba8a17e
Alexander Jung <104335080+AlexRamRam@users.noreply.github.com>
2023-04-14 16:13:00 -0400

    Add end-to-end integration test for 3PC EVM runner
    Signed-off-by: Alexander Jung <104335080+AlexRamRam@users.noreply.github.com>

---
+++ tests/integration/gtest_evm_jsonrpc_client.hpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2023 MIT Digital Currency Initiative,
+//
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef OPENCBDC_TEST_INTEGRATION_GTEST_EVM_JSONRPC_CLIENT_H_
+#define OPENCBDC_TEST_INTEGRATION_GTEST_EVM_JSONRPC_CLIENT_H_
+
+#include "3pc/agent/runners/evm/messages.hpp"
+#include "util/rpc/http/json_rpc_http_client.hpp"
+
+namespace cbdc::test {
+
+    class gtest_evm_jsonrpc_client : public cbdc::rpc::json_rpc_http_client {
+      public:
+        gtest_evm_jsonrpc_client(
+            std::vector<std::string> endpoints,
+            long timeout,
+            const std::shared_ptr<cbdc::logging::log>& log);
+
+        void send_transaction(const cbdc::threepc::agent::runner::evm_tx& tx,
+                              std::string& out_txid);
+
+        void get_transaction_receipt(const std::string& txid,
+                                     Json::Value& out_tx_receipt);
+
+        void get_balance(const evmc::address& addr,
+                         std::optional<evmc::uint256be>& out_balance);
+
+        [[nodiscard]] evmc::uint256be
+        get_transaction_count(const evmc::address& addr);
+
+      private:
+        static constexpr auto m_json_error_key = "error";
+        static constexpr auto m_json_result_key = "result";
+
+        void get_transaction_count_str_(const evmc::address& addr,
+                                        std::string& out_txcount_str);
+    };
+
+}
+
+#endif // OPENCBDC_TEST_INTEGRATION_GTEST_EVM_JSONRPC_CLIENT_H_
---
+++ tests/integration/sample_erc20_contract.hpp
@@ -0,0 +1,69 @@
+// Copyright (c) 2023 MIT Digital Currency Initiative,
+//
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef OPENCBDC_TEST_INTEGRATION_SAMPLE_ERC20_CONTRACT_H_
+#define OPENCBDC_TEST_INTEGRATION_SAMPLE_ERC20_CONTRACT_H_
+
+#include "3pc/agent/runners/evm/hash.hpp"
+#include "util/common/buffer.hpp"
+
+#include <evmc/evmc.hpp>
+
+/**
+ * This sample EVM contract bytecode and ABI interface have been copied from
+ * tools/bench/3pc/evm/contracts where the originating Solidity code looks as
+ * follows and was compiled using hardhat.
+
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract Token is ERC20 {
+    constructor() ERC20("Tokens", "TOK") {
+        _mint(msg.sender, 1000000000000000000000000); // 1M Coins with 18
+decimals
+    }
+}
+ */
+namespace cbdc::test::evm_contracts {
+    static constexpr size_t selector_size = 4;
+    static constexpr size_t param_size = 32;
+
+    static constexpr size_t address_param_offset
+        = 12; // in ABIs addresses are also 32 bytes
+    auto data_erc20_contract_bytecode() -> cbdc::buffer;
+
+    auto data_erc20_allowance(evmc::address owner, evmc::address spender)
+        -> cbdc::buffer;
+
+    auto data_erc20_approve(evmc::address spender, evmc::uint256be amount)
+        -> cbdc::buffer;
+
+    auto data_erc20_balance_of(evmc::address account) -> cbdc::buffer;
+
+    auto data_erc20_decimals() -> cbdc::buffer;
+
+    auto data_erc20_decrease_allowance(evmc::address spender,
+                                       evmc::uint256be subtracted_value)
+        -> cbdc::buffer;
+
+    auto data_erc20_increase_allowance(evmc::address spender,
+                                       evmc::uint256be added_value)
+        -> cbdc::buffer;
+
+    auto data_erc20_name() -> cbdc::buffer;
+
+    auto data_erc20_symbol() -> cbdc::buffer;
+
+    auto data_erc20_total_supply() -> cbdc::buffer;
+
+    auto data_erc20_transfer(evmc::address to, evmc::uint256be amount)
+        -> cbdc::buffer;
+
+    auto data_erc20_transfer_from(evmc::address from,
+                                  evmc::address to,
+                                  evmc::uint256be amount) -> cbdc::buffer;
+
+}
+
+#endif // OPENCBDC_TEST_INTEGRATION_SAMPLE_ERC20_CONTRACT_H_
