5cddc4db9824377fec5b9afa25d3cc9db2060774
Zee Khan <zek224@lehigh.edu>
2023-10-04 10:55:31 -0400

    hotfix 3

--- src/uhs/twophase/coordinator/testOracleDB.cpp
+++ src/uhs/twophase/coordinator/testOracleDB.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     OracleDB db;
-    if (OracleDB_Init(&db) == 0) {
+    if (OracleDB_init(&db) == 0) {
         std::cout << "OracleDB initialized successfully." << std::endl;
         // Call other functions as needed
         // ...
