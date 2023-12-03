20b1edd5513e1f8d92f4f59c536afeb52b1a66f1
Michael Maurer <maurer.mi@northeastern.edu>
2023-12-03 01:10:29 -0500

    Add a get_row function to parsec utils
    Adds a tool to get information directly from the
    shards, for debug and testing purposes.
    
    Signed-off-by: Michael Maurer <maurer.mi@northeastern.edu>

--- src/parsec/util.hpp
+++ src/parsec/util.hpp
@@ -24,7 +24,7 @@ namespace cbdc::parsec {
         /// Transaction semantics defined using Lua.
         lua,
         /// Ethereum-style transactions using EVM.
-        evm,
+        evm
     };
 
     /// Configuration parameters for a phase two system.
@@ -71,6 +71,20 @@ namespace cbdc::parsec {
                  broker::key_type key,
                  broker::value_type value,
                  const std::function<void(bool)>& result_callback) -> bool;
+
+    /// Asynchronously get the value stored at key from the cluster.
+    /// Intended for testing and administrative purposes.
+    /// \param broker broker to use for reading the row.
+    /// \param key key at which to read.
+    /// \param result_callback function to call on fetch success or
+    ///                        failure.
+    /// \return value stored at key
+    auto get_row(const std::shared_ptr<broker::interface>& broker,
+                 broker::key_type key,
+                 const std::function<void(
+                     cbdc::parsec::broker::interface::try_lock_return_type)>&
+                     result_callback)
+        -> cbdc::parsec::broker::interface::try_lock_return_type;
 }
 
 #endif
