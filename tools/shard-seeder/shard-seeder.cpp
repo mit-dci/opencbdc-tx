825d271f28beccd2ae1cb82f3a5b58a628ae1652
Sam Stuewe <stuewe@mit.edu>
2024-05-20 15:15:58 -0500

    feat: update shard-seeder tool for proofs
    cf. f7a2688ccf78df7fd0c3048e1310a488b6f527fc
    
    This was predominantly written by davidmag854, but includes some
    fundamental changes from me (and cannot easily be cherry-picked).
    
    Co-authored-by: davidmag854 <44822171+davidmag854@users.noreply.github.com>
    Signed-off-by: Sam Stuewe <stuewe@mit.edu>

--- tools/shard-seeder/shard-seeder.cpp
+++ tools/shard-seeder/shard-seeder.cpp
@@ -3,13 +3,17 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include "uhs/transaction/messages.hpp"
 #include "uhs/transaction/transaction.hpp"
 #include "uhs/transaction/validation.hpp"
 #include "uhs/transaction/wallet.hpp"
+#include "util/common/commitment.hpp"
 #include "util/common/config.hpp"
 #include "util/serialization/buffer_serializer.hpp"
 #include "util/serialization/format.hpp"
 #include "util/serialization/ostream_serializer.hpp"
+#include "uhs/twophase/locking_shard/locking_shard.hpp"
+#include "uhs/twophase/locking_shard/format.hpp"
 
 #include <algorithm>
 #include <chrono>
@@ -22,6 +26,11 @@ static constexpr int leveldb_buffer_size
     = 16 * 1024 * 1024; // 16MB can hold ~ 500K UHS_IDs
 static constexpr int write_batch_size
     = 450000; // well within the write buffer size
+
+/// should be twice the bitcount of the range-proof's upper bound
+///
+/// e.g., if proving things in the range [0, 2^64-1], it should be 128.
+static const inline auto generator_count = 128;
 
 auto get_2pc_uhs_key(const cbdc::hash_t& uhs_id) -> std::string {
     auto ret = std::string();
@@ -68,6 +77,24 @@ auto main(int argc, char** argv) -> int {
                     secp256k1_context_destroy_type>
         secp_context{secp256k1_context_create(SECP256K1_CONTEXT_NONE),
                      &secp256k1_context_destroy};
+
+    struct GensDeleter {
+        explicit GensDeleter(secp256k1_context* ctx) : m_ctx(ctx) {}
+
+        void operator()(secp256k1_bppp_generators* gens) const {
+            secp256k1_bppp_generators_destroy(m_ctx, gens);
+        }
+
+        secp256k1_context* m_ctx;
+    };
+
+    static std::unique_ptr<secp256k1_bppp_generators, GensDeleter>
+        bulletproof_gens{
+            secp256k1_bppp_generators_create(secp_context.get(),
+                                             generator_count),
+            GensDeleter(secp_context.get())};
+
+    cbdc::random_source rng(cbdc::config::random_source);
 
     auto pubkey = cbdc::pubkey_from_privkey(cfg.m_seed_privkey.value(),
                                             secp_context.get());
@@ -125,23 +152,38 @@ auto main(int argc, char** argv) -> int {
                                      res.ToString());
                         return;
                     }
-
-                    auto tx = wal.create_seeded_transaction(0).value();
                     auto batch_size = 0;
                     leveldb::WriteBatch batch;
                     for(size_t tx_idx = 0; tx_idx != num_utxos; tx_idx++) {
-                        tx.m_inputs[0].m_prevout.m_index = tx_idx;
+                        auto tx = wal.create_seeded_transaction(tx_idx).value();
                         cbdc::transaction::compact_tx ctx(tx);
-                        const cbdc::hash_t& output_hash = ctx.m_uhs_outputs[0];
+                        const cbdc::hash_t& output_hash
+                            = cbdc::transaction::calculate_uhs_id(
+                                ctx.m_outputs[0]);
                         if(output_hash[0] >= shard_start
                            && output_hash[0] <= shard_end) {
                             std::array<char, sizeof(output_hash)> hash_arr{};
                             std::memcpy(hash_arr.data(),
                                         output_hash.data(),
                                         sizeof(output_hash));
+                            static constexpr auto aux_size
+                                = sizeof(ctx.m_outputs[0].m_value_commitment);
+                            static constexpr auto rng_size
+                                = sizeof(ctx.m_outputs[0].m_range);
+                            std::array<char, aux_size + rng_size> proofs_arr{};
+
+                            std::memcpy(proofs_arr.data(),
+                                        ctx.m_outputs[0].m_value_commitment.data(),
+                                        aux_size);
+                            std::memcpy(proofs_arr.data() + aux_size,
+                                        ctx.m_outputs[0].m_range.data(),
+                                        rng_size);
                             leveldb::Slice hash_key(hash_arr.data(),
                                                     output_hash.size());
-                            batch.Put(hash_key, leveldb::Slice());
+                            leveldb::Slice ProofVal(proofs_arr.data(),
+                                                    proofs_arr.size());
+
+                            batch.Put(hash_key, ProofVal);
                             batch_size++;
                             if(batch_size >= write_batch_size) {
                                 db->Write(wopt, &batch);
@@ -161,14 +203,17 @@ auto main(int argc, char** argv) -> int {
                     // write dummy size
                     auto ser = cbdc::ostream_serializer(out);
                     ser << count;
-                    auto tx = wal.create_seeded_transaction(0).value();
                     for(size_t tx_idx = 0; tx_idx != num_utxos; tx_idx++) {
-                        tx.m_inputs[0].m_prevout.m_index = tx_idx;
+                        auto tx = wal.create_seeded_transaction(tx_idx).value();
                         cbdc::transaction::compact_tx ctx(tx);
-                        const cbdc::hash_t& output_hash = ctx.m_uhs_outputs[0];
-                        if(output_hash[0] >= shard_start
-                           && output_hash[0] <= shard_end) {
-                            ser << output_hash;
+                        const auto& compact_out = ctx.m_outputs[0];
+                        const auto& id
+                            = cbdc::transaction::calculate_uhs_id(compact_out);
+                        const auto& elem = cbdc::locking_shard::locking_shard::
+                            uhs_element{compact_out, 0};
+                        if(id[0] >= shard_start && id[0] <= shard_end) {
+                            ser << id;
+                            ser << elem;
                             count++;
                         }
                     }
